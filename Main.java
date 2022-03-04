572b8558dac909d335ee46e192a8882781eddfbe
BraxtonZ <100370778+BraxtonZ04@users.noreply.github.com>
2022-03-03 22:02:19 -0600

    Updated Main to add in the GUI and its functions

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
-class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
+
+    import javax.swing.*;
+    
+    public class Main {
+        private static final JFrame mainFrame = new JFrame("Checkers");
+        private static final GameCanvas gameCanvas = new GameCanvas();
+    
+        public static void main(String[] args) {
+            mainFrame.setSize(500, 500);
+            mainFrame.add(gameCanvas);
+    
+            mainFrame.setVisible(true);
+        }
     }
-}
+   
