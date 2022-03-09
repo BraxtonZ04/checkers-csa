271b0d86a11aa47f8ed0a664adc6ffec91ac4a45
BraxtonZ <100370778+BraxtonZ04@users.noreply.github.com>
2022-03-09 10:34:28 -0600

    Achinteya 3/9
    Lays out colors and stores information the the pieces themself and king or not

---
+++ Piece.java
@@ -0,0 +1,28 @@
+//package org.pleasval;
+
+public class Piece {
+    private boolean isKing;
+    private final int color; // 0 - white 1 - black
+
+    public Piece(int color) {
+        this.color = color;
+        this.isKing = false;
+    }
+
+    public Piece(int color, boolean isKing) {
+        this.color = color;
+        this.isKing = isKing;
+    }
+
+    public void setKing(boolean king) {
+        this.isKing = king;
+    }
+
+    public boolean isKing() {
+        return this.isKing;
+    }
+
+    public int getColor() {
+        return this.color;
+    }
+}
