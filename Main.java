42b70d4b2e33c8af680c32e9ad1dc0ced9d0ec5e
BraxtonZ <100370778+BraxtonZ04@users.noreply.github.com>
2022-03-09 10:31:58 -0600

    Updated all of us for new classes 3/9

--- Main.java
+++ Main.java
@@ -1,15 +1,18 @@
+//package org.pleasval;
 
-    import javax.swing.*;
-    
-    public class Main {
-        private static final JFrame mainFrame = new JFrame("Checkers");
-        private static final GameCanvas gameCanvas = new GameCanvas();
-    
-        public static void main(String[] args) {
-            mainFrame.setSize(500, 500);
-            mainFrame.add(gameCanvas);
-    
-            mainFrame.setVisible(true);
-        }
+import javax.swing.*;
+
+public class Main {
+    private static final JFrame mainFrame = new JFrame("Checkers");
+    private static final Board gameBoard = new Board();
+
+    public static void main(String[] args) {
+        mainFrame.setSize(650, 650);
+
+        mainFrame.setLocationByPlatform(true);
+        gameBoard.setupBoard();
+        mainFrame.add(gameBoard.getBoardRenderer());
+
+        mainFrame.setVisible(true);
     }
-   
+}
