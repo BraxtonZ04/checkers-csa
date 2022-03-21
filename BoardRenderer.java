e2291c787430bb2edc5c8e66f153de6f9ee99e86
BraxtonZ <100370778+BraxtonZ04@users.noreply.github.com>
2022-03-21 10:26:44 -0500

    Jackson 3/21
    Updated render based on board update from danny

--- BoardRenderer.java
+++ BoardRenderer.java
@@ -52,9 +52,6 @@ public class BoardRenderer extends JPanel {
         int colX = x / SLOT_SIZE;
         int rowY = y / SLOT_SIZE;
 
-        this.selectedPieceX = colX;
-        this.selectedPieceY = rowY;//TODO
-
         System.out.println(colX + " " + rowY);
 
         return this.board.getPieceAt(colX, rowY);
@@ -67,23 +64,32 @@ public class BoardRenderer extends JPanel {
         System.out.println("Mouse X: " + x + " Mouse Y: " + y);
 
         Piece piece = calculatePieceAtMouse(x, y);
+        int[] coordinates = this.calculatePositionAtMouse(x, y);
 
         boolean selectPiece = piece != null && this.board.getTurn() == piece.getColor();
         if (selectPiece) {
             this.selectedPiece = piece;
+            this.selectedPieceX = coordinates[0];
+            this.selectedPieceY = coordinates[1];//TODO
         }
 
         if (this.selectedPiece != null && piece == null) { // selected empty square
             this.possibleMoves = this.board.calculatePossibleMoves(this.selectedPieceX, this.selectedPieceY);
+            System.out.println(this.selectedPieceX + " " + this.selectedPieceY);
 
             for (Move move : possibleMoves) {
-                if (move.getX() == x && move.getY() == y && move.canMove()){
+                if (move.getX() == coordinates[0] && move.getY() == coordinates[1] && move.canMove()){
+                    System.out.println("tried to move");
+
                     this.board.setPieceAt(this.selectedPieceX, this.selectedPieceY, null);
 
-                    int[] coordinates = this.calculatePositionAtMouse(x, y);
                     this.board.setPieceAt(coordinates[0], coordinates[1], this.selectedPiece);
                 }
             }
+
+            this.selectedPiece = null;
+            this.selectedPieceY = 0;
+            this.selectedPieceX = 0;
         }
 
         //this.board.setPieceAt(5, 5, new Piece(0));
@@ -91,12 +97,17 @@ public class BoardRenderer extends JPanel {
 
     @Override
     public void paintComponent(Graphics g) {
+        Graphics2D graphics2D = (Graphics2D) g;
+
+        graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        graphics2D.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
         for (int y = 0; y < 8; y++) {
             for (int x = 0; x < 8; x++) {
                 if ((x + y) % 2 == 0) {
-                    g.setColor(Color.WHITE);
+                    graphics2D.setColor(Color.WHITE);
                 } else {
-                    g.setColor(Color.BLACK);
+                    graphics2D.setColor(Color.BLACK);
                 }
 
                 if (this.selectedPiece != null) {
@@ -104,37 +115,37 @@ public class BoardRenderer extends JPanel {
 
                     for (Move move : possibleMoves) {
                         if (move.getX() == x && move.getY() == y && move.canMove()){
-                            g.setColor(Color.GREEN);
+                            graphics2D.setColor(Color.GREEN);
                         }
                     }
                 }
-                g.fillRect(x * SLOT_SIZE, y * SLOT_SIZE, SLOT_SIZE, SLOT_SIZE);
+                graphics2D.fillRect(x * SLOT_SIZE, y * SLOT_SIZE, SLOT_SIZE, SLOT_SIZE);
 
                 Piece piece = this.board.getPieceAt(x, y);
                 if (piece != null) {
                     if (piece == this.selectedPiece) {
-                        g.setColor(Color.YELLOW);
+                        graphics2D.setColor(Color.YELLOW);
                     } else if (piece.getColor() == 0) {
-                        g.setColor(Color.RED);
+                        graphics2D.setColor(Color.RED);
                     } else if (piece.getColor() == 1) {
-                        g.setColor(Color.BLUE);
+                        graphics2D.setColor(Color.BLUE);
                     }
 
-                    g.fillOval(x * SLOT_SIZE + (PIECE_PADDING / 2), y * SLOT_SIZE + (PIECE_PADDING / 2), SLOT_SIZE - PIECE_PADDING, SLOT_SIZE - PIECE_PADDING);
+                    graphics2D.fillOval(x * SLOT_SIZE + (PIECE_PADDING / 2), y * SLOT_SIZE + (PIECE_PADDING / 2), SLOT_SIZE - PIECE_PADDING, SLOT_SIZE - PIECE_PADDING);
                 }
 
 
                 if ((x + y) % 2 == 0) { // opposite color for text
-                    g.setColor(Color.BLACK);
+                    graphics2D.setColor(Color.BLACK);
                 } else {
-                    g.setColor(Color.WHITE);
+                    graphics2D.setColor(Color.WHITE);
                 }
 
                 int slot = x + (y * 8);
                 double cellTextX = ((x + 1) * SLOT_SIZE) - (SLOT_SIZE / 2.0);
                 double cellTextY = ((y + 1) * SLOT_SIZE) - (SLOT_SIZE / 2.0);
 
-                g.drawString(String.valueOf(slot), (int) cellTextX, (int) cellTextY);
+                graphics2D.drawString(String.valueOf(slot), (int) cellTextX, (int) cellTextY);
             }
         }
     }
