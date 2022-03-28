3ab7706f2d94d677f8db748ed94e0f42ce43d796
BraxtonZ <100370778+BraxtonZ04@users.noreply.github.com>
2022-03-28 10:09:02 -0500

    Braxton 3/28
    Takes location and looks at piece can be captured or not and if move is available

--- Move.java
+++ Move.java
@@ -2,13 +2,34 @@
 
 public class Move {
     private final boolean canMove;
+    private boolean capturePiece;
+
     private final int x;
     private final int y;
 
-    public Move(int x, int y, boolean canMove) {
+    private int x2 = -1; // taking piece
+    private int y2 = -1;
+
+    private final int x3; // final position
+    private final int y3;
+
+    public Move(int x, int y, int x3, int y3, boolean canMove) {
         this.x = x;
         this.y = y;
+        this.x3 = x3;
+        this.y3 = y3;
         this.canMove = canMove;
+    }
+
+    public Move(int x, int y, int x2, int y2, int x3, int y3, boolean canMove) {
+        this.x = x;
+        this.y = y;
+        this.x2 = x2;
+        this.y2 = y2;
+        this.x3 = x3;
+        this.y3 = y3;
+        this.canMove = canMove;
+        this.capturePiece = true;
     }
 
     public int getX() {
@@ -19,7 +40,27 @@ public class Move {
         return this.y;
     }
 
+    public int getX2() {
+        return this.x2;
+    }
+
+    public int getY2() {
+        return this.y2;
+    }
+
+    public int getX3() {
+        return this.x3;
+    }
+
+    public int getY3() {
+        return this.y3;
+    }
+
     public boolean canMove() {
         return this.canMove;
+    }
+
+    public boolean canCapturePiece() {
+        return capturePiece;
     }
 }
