153fbe251967eeb92a8400def550d4adea5e16ad
BraxtonZ <100370778+BraxtonZ04@users.noreply.github.com>
2022-03-28 10:07:36 -0500

    All 3/28
    Added on our own based on what we did alone

--- Main.java
+++ Main.java
@@ -1,18 +1,22 @@
 //package org.pleasval;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     private static final JFrame mainFrame = new JFrame("Checkers");
-    private static final Board gameBoard = new Board();
+    private static final Board gameBoard = new Board(true);
 
     public static void main(String[] args) {
-        mainFrame.setSize(650, 650);
+        BoardRenderer renderer = gameBoard.getBoardRenderer();
+        int slotSize = renderer.getSLOT_SIZE();
+        int boxSize = renderer.getBoxWidth();
 
-        mainFrame.setLocationByPlatform(true);
-        gameBoard.setupBoard();
-        mainFrame.add(gameBoard.getBoardRenderer());
-
+        mainFrame.add(renderer);
+        mainFrame.setSize(new Dimension((slotSize * 8) + boxSize + 15, (slotSize * 8) + 39));
+        mainFrame.setResizable(false);
         mainFrame.setVisible(true);
+
+        gameBoard.setupBoard();
     }
 }
